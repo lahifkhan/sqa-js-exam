56621c20d327bbad01ce4846aca7dc0bfcbaff16
lahifkhan <lahifkhan52@gmail.com>
2026-05-16 22:14:55 +0600

    second question solution

---
+++ sequentialGranularity.js
@@ -0,0 +1,7 @@
+const temporalCode = 20240628;
+const s = temporalCode.toString();
+if (s.length >= 2) {
+  console.log("Security Level: Omega");
+} else {
+  console.log("Security Level: Sigma");
+}
